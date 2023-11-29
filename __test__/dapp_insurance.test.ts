0b238fca6b4fda4c966d83b516354aaff7048253
Evert Diaz <evertdiazb@gmail.com>
2023-11-29 17:30:55 -0500

    Project initialised with AlgoKit CLI using template: https://github.com/algorand-devrel/tealscript-algokit-template.git

---
+++ __test__/dapp_insurance.test.ts
@@ -0,0 +1,41 @@
+import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
+import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import { DappInsuranceClient } from '../contracts/clients/DappInsuranceClient';
+
+const fixture = algorandFixture();
+
+let appClient: DappInsuranceClient;
+
+describe('DappInsurance', () => {
+  beforeEach(fixture.beforeEach);
+
+  beforeAll(async () => {
+    await fixture.beforeEach();
+    const { algod, testAccount } = fixture.context;
+
+    appClient = new DappInsuranceClient(
+      {
+        sender: testAccount,
+        resolveBy: 'id',
+        id: 0,
+      },
+      algod
+    );
+
+    await appClient.create.createApplication({});
+  });
+
+  test('sum', async () => {
+    const a = 13;
+    const b = 37;
+    const sum = await appClient.doMath({ a, b, operation: 'sum' });
+    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
+  });
+
+  test('difference', async () => {
+    const a = 13;
+    const b = 37;
+    const diff = await appClient.doMath({ a, b, operation: 'difference' });
+    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
+  });
+});
